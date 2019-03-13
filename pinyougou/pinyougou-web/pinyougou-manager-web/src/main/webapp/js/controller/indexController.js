2a94af02486d2368472d6844bbecef6d3c316f6a
chenhaoxian <773885329@qq.com>
2019-03-13 16:10:13 +0800

    删除

--- pinyougou/pinyougou-web/pinyougou-manager-web/src/main/webapp/js/controller/indexController.js
+++
@@ -1,9 +0,0 @@
-app.controller('indexController', function ($scope, baseService) {
-    $scope.showLoginName = function () {
-        baseService.sendGet("/showLoginName")
-            .then(function (response) {
-                // 获取响应数据
-                $scope.loginName = response.data.loginName;
-            })
-    };
-});
